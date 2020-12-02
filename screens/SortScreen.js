e68170430cff37f830b02a62485881d44c93eac3
Dylan <dyevki@gmail.com>
2020-12-02 10:18:08 +0000

    Start of application

---
+++ screens/SortScreen.js
@@ -0,0 +1,14 @@
+import React from 'React';
+import { View, Text } from 'react-native';
+
+const SortScreen = props => {
+    return (
+        <View style={}
+
+    )
+
+};
+
+const styles = StyleSheet.create({});
+
+export default SortScreen;
